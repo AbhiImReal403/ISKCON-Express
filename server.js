db8c0f38867567815ca1d2f0257711034f15f706
Abhilash <abhilashanil40404@gmail.com>
2023-01-21 11:20:13 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use('/statics',express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000)
+console.log('listening on port 3000')
+
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
